9c0050b86275fcce6fa3fd41595a13ad2d1dbb08
Web-Tushar <tusharimranvip895@gmail.com>
2024-01-15 01:11:34 +0600

    first comment

---
+++ script.js
@@ -0,0 +1,40 @@
+let input = document.querySelector(".inputbox");
+let upperbtn = document.querySelector(".upperbtn");
+let lowerbtn = document.querySelector(".lowerbtn");
+// let lengthbtn = document.querySelector(".lengthbtn");
+let result = document.querySelector(".result");
+let repeatbtn = document.querySelector(".repeatbtn");
+let lengthbtn =document.querySelector(".length")
+let resetbtn = document.querySelector(".reset");
+
+
+upperbtn.addEventListener("click",function(){
+
+     result.innerHTML = input.value.toUpperCase();
+ 
+ })
+ 
+ lowerbtn.addEventListener("click" , function(){
+ 
+     result.innerHTML = input.value.toLowerCase();
+ 
+ })
+ 
+ repeatbtn.addEventListener("click" , function(){
+ 
+     result.innerHTML = input.value.repeat(20);
+ 
+ })
+ lengthbtn.addEventListener("click",function(){
+
+     result.innerHTML = input.value.length;
+
+ })
+
+
+ resetbtn.addEventListener("click" , function(){
+ 
+     result.innerHTML = "";
+     input.value = "";
+ 
+ })
